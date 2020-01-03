cc079d0a8563f166c5e493f63df7fb930f9208fd
grassfuel <50146395+grassfuel@users.noreply.github.com>
2020-01-04 01:08:16 +0900

    bar update
    Add a slide bar

--- script/koko.js
+++ script/koko.js
@@ -1,7 +1,52 @@
+windowsize = document.getElementById("windowsize");
+sidehandle = document.getElementById("sidehandle");
+conveyer = document.getElementById("conveyer");
+sidehandle.style.top = "0px";
+handlestyle = sidehandle.offsetHeight / (windowsize.clientHeight - sidehandle.offsetHeight);
+sidehandle.ondragstart = function(){
+    return false;
+};
+sidehandle.onmousedown = function(e){
+    fg = event.clientY - parseInt(sidehandle.style.top,10);
+    function getXY(e){
+        var y = event.clientY;
+        if(parseInt(sidehandle.style.top,10) >= 0 && parseInt(sidehandle.style.top,10) + sidehandle.offsetHeight <= windowsize.clientHeight){
+            handlestyle = sidehandle.offsetHeight / (windowsize.clientHeight - sidehandle.offsetHeight);
+            sidehandle.style.top = y-fg + "px";
+            conveyer.style.top = windowsize.clientHeight - parseInt(sidehandle.style.top,10) - parseInt(sidehandle.style.top,10) * handlestyle + "px";
+//            conveyer.style.top = windowsize.clientHeight - y + "px";
+        }
+        else{
+            document.removeEventListener("mousemove",getXY);
+            if(parseInt(sidehandle.style.top,10) < 0){
+                sidehandle.style.top = 0 + "px";
+            }
+            if(parseInt(sidehandle.style.top,10) + sidehandle.offsetHeight > windowsize.clientHeight){
+                sidehandle.style.top = windowsize.clientHeight - sidehandle.offsetHeight + "px";
+            }
+        }
+    }
+    document.addEventListener("mousemove",getXY);
+    sidehandle.onmouseup = function(){
+        document.removeEventListener("mousemove",getXY);
+    }
+}
+
 function konnitiha(){
     console.log("よみこめました");
+    console.log(windowsize.clientHeight);
 }
-function yomidasi(){
+
+/*
+function getXY(e){
+    y = e.clientY - fy;
+    fy = e.clientY;
+    if(parseInt(sidehandle.style.top,10) >= 0 && parseInt(sidehandle.style.top,10) + sidehandle.offsetHeight <= windowsize.clientHeight){
+            sidehandle.style.top = parseInt(sidehandle.style.top) + y + "px";
+            conveyer.style.top = windowsize.clientHeight - y - 0.1 + "px";
+    }
 }
+*/
+
+
 konnitiha();
-yomidasi();
